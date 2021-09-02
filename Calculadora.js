1b53873240f28a5ff6e180062040f3a31c46deb2
Sebastian Daza <jsebastiandaza2002@gmail.com>
2021-09-01 23:04:34 -0500

    primero

---
+++ Calculadora.js
@@ -0,0 +1,14 @@
+class Calculadora {
+    sumar(number1, number2) {
+        return number1 + number2;
+    }
+    restar(number1, number2){
+        return number1 - number2;
+    }
+    multiplicar(number1, number2) {
+        return number1 * number2;
+    }
+    dividir(number1, number2) {
+        return number1 / number2;
+    }
+}
